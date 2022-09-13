8073702fa6d35bac997e9f90305d9b6e43918d5c
haroon-islootech <haroon.islootech@gmail.com>
2022-09-13 12:00:04 +0500

    2 Tabs Added in Site Configuration

--- database/migrations/2022_07_19_132437_create_site_configrations_table.php
+++ database/migrations/2022_07_19_132437_create_site_configrations_table.php
@@ -19,8 +19,19 @@ return new class extends Migration
             $table->tinyInteger('site_max_floors')->default(0);
             $table->tinyInteger('site_token_percentage')->default(5);
             $table->tinyInteger('site_down_payment_percentage')->default(25);
+
             $table->string('floor_prefix', 5)->default('F');
+
             $table->tinyInteger('unit_number_digits')->default(2);
+
+            $table->tinyInteger('salesplan_validity_days')->default(7);
+            $table->tinyInteger('salesplan_installment_days')->default(90);
+
+            $table->string('others_bank_name')->nullable();
+            $table->string('others_bank_account_name')->nullable();
+            $table->string('others_bank_account_no')->nullable();
+
+
             $table->timestamps();
             $table->softDeletes();
         });

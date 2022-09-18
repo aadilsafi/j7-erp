04cf435b4740ef1199ccc3f57a6f05116d09ddb4
haroon-islootech <haroon.islootech@gmail.com>
2022-09-18 21:29:57 +0500

    Minor Potatos Fixed

--- resources/views/app/sites/index.blade.php
+++ resources/views/app/sites/index.blade.php
@@ -91,6 +91,11 @@
                     cancelButtonText: '{{ __('lang.commons.no_cancel') }}',
                     confirmButtonText: '{{ __('lang.commons.yes_delete') }}',
                     confirmButtonClass: 'btn-danger',
+                    buttonsStyling: false,
+                    customClass: {
+                        confirmButton: 'btn btn-relief-outline-danger waves-effect waves-float waves-light me-1',
+                        cancelButton: 'btn btn-relief-outline-success waves-effect waves-float waves-light me-1'
+                    },
                 }).then((result) => {
                     if (result.isConfirmed) {
                         $('#sites-table-form').submit();

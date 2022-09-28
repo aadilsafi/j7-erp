acd661a3155f3ae0c40d4679c8d46247d4932441
Iftikhar Shah <iftikhar.islootech@gmail.com>
2022-09-28 16:11:07 +0500

    Receipt Perview changes

--- resources/views/app/sites/receipts/preview.blade.php
+++ resources/views/app/sites/receipts/preview.blade.php
@@ -203,12 +203,54 @@
                                         value="{{ \Carbon\Carbon::parse($receipt->created_at)->format('F j, Y') }}"
                                         readonly />
                                 </div>
+                                @if ($receipt->mode_of_payment == 'Cheque')
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-2 position-relative">
+                                    <label class="form-label fs-5" for="unit_type">Check Number</label>
+                                    <input type="text" class="form-control form-control-lg" id="unit_type"
+                                        name="unit[type]" placeholder="Check Number"
+                                        value="{{ $receipt->cheque_no }}" readonly />
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-2 position-relative">
+                                    <label class="form-label fs-5" for="unit_type">Bank Name</label>
+                                    <input type="text" class="form-control form-control-lg" id="unit_type"
+                                        name="unit[type]" placeholder="Bank Name"
+                                        value="{{ $receipt->bank_details }}"
+                                        readonly />
+                                </div>
+                                @endif
+                              
+                                @if ($receipt->mode_of_payment == 'Online')
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-2 position-relative">
+                                    <label class="form-label fs-5" for="unit_type">Transaction No</label>
+                                    <input type="text" class="form-control form-control-lg" id="unit_type"
+                                        name="unit[type]" placeholder="Transaction No"
+                                        value="{{ $receipt->online_instrument_no }}" readonly />
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-2 position-relative">
+                                    <label class="form-label fs-5" for="unit_type">Transaction Date</label>
+                                    <input type="text" class="form-control form-control-lg" id="unit_type"
+                                        name="unit[type]" placeholder="Transaction Date"
+                                        value="{{ $receipt->transaction_date }}"
+                                        readonly />
+                                </div>
+                                @endif
+
+                                @if ($receipt->mode_of_payment == 'Other')
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-2 position-relative">
+                                    <label class="form-label fs-5" for="unit_type">Other Payment Mode</label>
+                                    <input type="text" class="form-control form-control-lg" id="unit_type"
+                                        name="unit[type]" placeholder="Other Payment Mode"
+                                        value="{{ $receipt->other_value }}" readonly />
+                                </div>
+                                @endif
 
                                 <div class="col-lg-12 col-md-12 col-sm-12 position-relative">
                                     <label class="form-label fs-5" for="floor_no">AMOUNT IN WORDS</label>
                                     <input type="text" class="form-control form-control-lg" id="floor_no"
                                         name="unit[floor_no]" placeholder=""
-                                        value="{{ numberToWords($receipt->amount_in_numbers) }} only." readonly />
+                                        value="{{ \Str::title(numberToWords($receipt->amount_in_numbers)) }} only." readonly />
                                 </div>
 
                                 <div class="col-lg-12 col-md-12 col-sm-12 position-relative mt-1">

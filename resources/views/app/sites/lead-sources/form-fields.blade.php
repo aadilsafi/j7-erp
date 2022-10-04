9098004182c6c59cb72ae964e6a5cfad84285f7f
haroon-islootech <haroon.islootech@gmail.com>
2022-10-04 08:43:48 +0500

    LeadSource Style Fixed

--- resources/views/app/sites/lead-sources/form-fields.blade.php
+++ resources/views/app/sites/lead-sources/form-fields.blade.php
@@ -1,11 +1,15 @@
-<div class="row mb-1">
-    <div class="col-lg-12 col-md-12 col-sm-12 position-relative">
-        <label class="form-label fs-5" for="lead-source-name">Name</label>
-        <input type="text" class="form-control form-control-lg @error('lead_source_name') is-invalid @enderror"
-            id="lead-source-name" name="lead_source_name" placeholder="Name"
-            value="{{ isset($leadSource) ? $leadSource->name : old('lead_source_name') }}" />
-        @error('lead_source_name')
-            <div class="invalid-tooltip">{{ $message }}</div>
-        @enderror
+<div class="card" style="border: 2px solid #7367F0; border-style: dashed; border-radius: 0;">
+    <div class="card-body">
+        <div class="row mb-1">
+            <div class="col-lg-12 col-md-12 col-sm-12 position-relative">
+                <label class="form-label fs-5" for="lead-source-name">Name</label>
+                <input type="text" class="form-control form-control-lg @error('lead_source_name') is-invalid @enderror"
+                    id="lead-source-name" name="lead_source_name" placeholder="Name"
+                    value="{{ isset($leadSource) ? $leadSource->name : old('lead_source_name') }}" />
+                @error('lead_source_name')
+                    <div class="invalid-tooltip">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
     </div>
 </div>

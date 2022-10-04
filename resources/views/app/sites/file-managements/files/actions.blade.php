8a0dd792ccceaf5e69c611712dd9eae6aaa6db92
Aadil Safi <aadilxafi@gmail.com>
2022-10-04 15:44:42 +0500

    refund changes

--- resources/views/app/sites/file-managements/files/actions.blade.php
+++ resources/views/app/sites/file-managements/files/actions.blade.php
@@ -1,10 +1,16 @@
-<div class="d-flex justify-content-cetner align-items-center">
-    @can('sites.file-managements.customers.units.files.index')
-        <a class="btn btn-relief-outline-primary waves-effect waves-float waves-light" style="margin: 5px"
-            data-bs-toggle="tooltip" data-bs-placement="top" title="View File Details"
-            href="{{ route('sites.file-managements.customers.units.files.index', ['site_id' => encryptParams($site_id), 'customer_id' => encryptParams($customer_id), 'unit_id' => encryptParams($unit_id)]) }}">
-            <i class="bi bi-file-person-fill" style="font-size: 1.1rem" class="m-10"></i>
-        </a>
+<div class="d-flex justify-content-cetner align-items-center text-nowrap">
+    @can('sites.file-managements.view-files')
+        @if (Route::current()->getName() == 'sites.file-managements.view-files')
+            <a class="btn btn-relief-outline-primary waves-effect waves-float waves-light" style="margin: 5px"
+                data-bs-toggle="tooltip" data-bs-placement="top" title="View File Details"
+                href="{{ route('sites.file-managements.customers.units.files.index', ['site_id' => encryptParams($site_id), 'customer_id' => encryptParams($customer_id), 'unit_id' => encryptParams($unit_id)]) }}">
+                <i class="bi bi-file-person-fill" style="font-size: 1.1rem" class="m-10"></i>
+            </a>
+        @else
+        @endif
+        {{-- @if (Route::current()->getName() == 'sites.file-managements.customers.units.files.create')
+            <span class="badge badge-glow bg-success">File Already Exist</span>
+        @endif --}}
     @endcan
     @if (Route::current()->getName() == 'sites.file-managements.file-refund.index')
         @php

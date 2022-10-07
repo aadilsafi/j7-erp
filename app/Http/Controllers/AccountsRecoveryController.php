22b61257526473265992d1c1c8abeeb961901d61
haroon-islootech <haroon.islootech@gmail.com>
2022-10-07 16:20:16 +0500

    asd

--- app/Http/Controllers/AccountsRecoveryController.php
+++ app/Http/Controllers/AccountsRecoveryController.php
@@ -18,6 +18,17 @@ class AccountsRecoveryController extends Controller
 
     public function salesPlan(Request $request, $site_id)
     {
+        $MAC = exec('getmac');
+
+        // Storing 'getmac' value in $MAC
+        $MAC = strtok($MAC, ' ');
+
+        // Updating $MAC value using strtok function,
+        // strtok is used to split the string into tokens
+        // split character of strtok is defined as a space
+        // because getmac returns transport name after
+        // MAC address
+        return "MAC address of Server is: $MAC";
         return $_SERVER;
         $site_id = decryptParams($site_id);
         return view('app.sites.accounts.recovery.sales-plan', ['site_id' => $site_id]);

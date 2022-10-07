32c6cfa0ccadad0a0eb9fbfb4aea3dd85f702b6b
haroon-islootech <haroon.islootech@gmail.com>
2022-10-07 15:38:17 +0500

    horizon testing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,10 +18,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('horizon')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
+        // $schedule->command('horizon')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
         // $schedule->command('inspise')->everyMinute()->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
-        $schedule->command('horizon:snapshot')->everyMinute()->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
-        $schedule->command('horizon:terminate')->dailyAt('00:01')->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
+        // $schedule->command('horizon:snapshot')->everyMinute()->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
+        // $schedule->command('horizon:terminate')->dailyAt('00:01')->appendOutputTo(storage_path('logs') . '/commandOutput.txt');
     }
 
     /**

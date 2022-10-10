65b1b5a5f67b60b3b6d10dbfa1885921479cfd64
haroon-islootech <haroon.islootech@gmail.com>
2022-10-10 12:20:44 +0500

    J7-326

--- resources/views/app/sites/file-managements/customers/units/actions.blade.php
+++ resources/views/app/sites/file-managements/customers/units/actions.blade.php
@@ -1,6 +1,7 @@
 <div class="d-flex justify-content-cetner align-items-center">
+
     @can('sites.file-managements.customers.units.files.index')
-        @if (isset($file_Action_id) && $file_Action_id != 1)
+        @if (isset($isShowFileCreationButton) && $isShowFileCreationButton)
             <a class="btn btn-relief-outline-primary waves-effect waves-float waves-light" style="margin: 5px"
                 data-bs-toggle="tooltip" data-bs-placement="top" title="Create Files"
                 href="{{ route('sites.file-managements.customers.units.files.index', ['site_id' => encryptParams($site_id), 'customer_id' => encryptParams($customer_id), 'unit_id' => encryptParams($unit_id)]) }}">
@@ -9,7 +10,7 @@
         @else
             <a class="btn btn-relief-outline-primary waves-effect waves-float waves-light" style="margin: 5px"
                 data-bs-toggle="tooltip" data-bs-placement="top" title="View File"
-                href="{{ route('sites.file-managements.customers.units.files.show', ['site_id' => encryptParams($site_id), 'customer_id' => encryptParams($customer_id), 'unit_id' => encryptParams($unit_id)]) }}">
+                href="{{ route('sites.file-managements.customers.units.files.show', ['site_id' => encryptParams($site_id), 'customer_id' => encryptParams($customer_id), 'unit_id' => encryptParams($unit_id), 'file_id' => encryptParams($file_id)]) }}">
                 <i class="bi bi-file-person-fill" style="font-size: 1.1rem" class="m-10"></i>
             </a>
         @endif

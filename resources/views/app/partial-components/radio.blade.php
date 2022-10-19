f003662964846fa45c9063c152dc58dd92423ea7
haroon-islootech <haroon.islootech@gmail.com>
2022-10-19 18:12:00 +0500

    asd

--- resources/views/app/partial-components/radio.blade.php
+++ resources/views/app/partial-components/radio.blade.php
@@ -6,7 +6,7 @@
     <div class="col">
         <h5>{{ $label }}</h5>
     </div>
-    @foreach ($values as $key => $value)
+    @foreach ($values ?? [] as $key => $value)
         <div class="col">
             <div class="form-check form-check-inline">
                 <label class="form-check-label" for="{{ $key }}">{{ $value }} <span

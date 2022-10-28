f7c3c7810ced70593feb3ebb7d2eaf127573ef55
Aadil Safi <aadilxafi@gmail.com>
2022-10-28 17:46:23 +0500

    commit

--- app/Services/Stakeholder/StakeholderService.php
+++ app/Services/Stakeholder/StakeholderService.php
@@ -12,6 +12,7 @@ use App\Utils\Enums\StakeholderTypeEnum;
 use App\Services\Stakeholder\Interface\StakeholderInterface;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class StakeholderService implements StakeholderInterface
@@ -74,7 +75,8 @@ class StakeholderService implements StakeholderInterface
                 foreach ($inputs['attachment'] as $attachment) {
                     $stakeholder->addMedia($attachment)->toMediaCollection('stakeholder_cnic');
                 }
-                changeImageDirectoryPermission();
+                $returnValue = changeImageDirectoryPermission();
+                Log::info("changeImageDirectoryPermission => " . $returnValue);
             }
 
             if (isset($inputs['contact-persons']) && count($inputs['contact-persons']) > 0) {

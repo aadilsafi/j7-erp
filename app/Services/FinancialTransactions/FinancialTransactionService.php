677af92811da15988225e91b6a594c0a88e8ebef
Aadil Safi <aadilxafi@gmail.com>
2022-10-31 17:51:24 +0500

    commit

--- app/Services/FinancialTransactions/FinancialTransactionService.php
+++ app/Services/FinancialTransactions/FinancialTransactionService.php
@@ -222,12 +222,12 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $this->makeFinancialTransaction($receipt->site_id, $cashAccount, 2, $receipt->sales_plan_id, 'debit', $receipt->amount_in_numbers, NatureOfAccountsEnum::RECEIPT_VOUCHER, $receipt->id);
 
             // Customer AR Transaction
-            $customerAccount = collect($receipt->salesPlan->stakeholder->stakeholder_types)->where('type', 'C')->all()[0]->receivable_account;
-            $customerAccount = collect($customerAccount)->where('unit_id', $receipt->unit_id)->all();
+            $customerAccount = collect($receipt->salesPlan->stakeholder->stakeholder_types)->where('type', 'C')->all();
 
             if (count($customerAccount) < 1) {
                 throw new GeneralException('Customer Account is not defined. Please define customer account first.');
             }
+            $customerAccount = collect($customerAccount[0]->receivable_account)->where('unit_id', $receipt->unit_id)->all();
             $customerAccount = $customerAccount[0];
             $this->makeFinancialTransaction($receipt->site_id, $customerAccount['account_code'], 2, $receipt->sales_plan_id, 'credit', $receipt->amount_in_numbers, NatureOfAccountsEnum::RECEIPT_VOUCHER, $receipt->id);
             // dd($customerAccount, $cashAccount, $receipt);

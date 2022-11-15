397687e73b1bb3ef3f6d9b39c15a5e3d08c4052b
Aadil Safi <aadilxafi@gmail.com>
2022-11-15 18:08:07 +0500

    bug fixes

--- app/Services/FinancialTransactions/FinancialTransactionService.php
+++ app/Services/FinancialTransactions/FinancialTransactionService.php
@@ -29,7 +29,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
 
 
                 $origin_number = AccountLedger::where('account_action_id',1)->get();
-                if(isset($origin_number->origin_number)){
+                if(isset($origin_number)){
                     $origin_number = collect($origin_number)->last();
                     $origin_number = $origin_number->origin_number + 1;
                 }
@@ -73,7 +73,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
 
             $origin_number = AccountLedger::where('account_action_id',8)->get();
 
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = $origin_number->origin_number + 1;
             }
@@ -294,7 +294,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $receipt = (new Receipt())->find($receipt_id);
 
             $origin_number = AccountLedger::where('account_action_id',2)->get();
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = $origin_number->origin_number + 1;
             }
@@ -344,7 +344,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $stakeholder = Stakeholder::where('cnic', $receipt->cnic)->first();
             $stakeholderType = StakeholderType::where('stakeholder_id', $stakeholder->id)->where('type', 'C')->first();
             $origin_number = AccountLedger::where('account_action_id',9)->get();
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = (int)$origin_number->origin_number + 1;
             }
@@ -417,7 +417,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $receipt = (new Receipt())->find($receipt_id);
             $bankAccount = $receipt->bank->account_number;
             $origin_number = AccountLedger::where('account_action_id',12)->get();
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = (int)$origin_number->origin_number + 1;
             }
@@ -460,7 +460,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $onlyProfitAmount = $file_buy_back->amount_profit;
 
             $origin_number = AccountLedger::where('account_action_id',3)->get();
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = (int)$origin_number->origin_number + 1;
             }
@@ -550,7 +550,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $salesPlanRemainingAmount = (int)$sales_plan->total_price - (int)$refunded_amount;
 
             $origin_number = AccountLedger::where('account_action_id',6)->get();
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = (int)$origin_number->origin_number + 1;
             }
@@ -634,7 +634,7 @@ class FinancialTransactionService implements FinancialTransactionInterface
             $sales_plan = SalesPlan::find($fileTitleTransfer->sales_plan_id);
             $receipt = Receipt::where('sales_plan_id', $fileTitleTransfer->sales_plan_id)->first();
             $origin_number = AccountLedger::where('account_action_id',7)->get();
-            if(isset($origin_number->origin_number)){
+            if(isset($origin_number)){
                 $origin_number = collect($origin_number)->last();
                 $origin_number = (int)$origin_number->origin_number + 1;
             }

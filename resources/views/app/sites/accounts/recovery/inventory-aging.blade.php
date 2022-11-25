7cb4341199c82d699569f73d7ce15ba6aa128d48
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2022-11-25 17:41:30 +0500

    updates

--- resources/views/app/sites/accounts/recovery/inventory-aging.blade.php
+++ resources/views/app/sites/accounts/recovery/inventory-aging.blade.php
@@ -65,15 +65,15 @@
     </p>
 
     <div class="row">
-        <div class="col-12">
+        {{-- <div class="col-12"> --}}
 
-            <div class="row">
+            {{-- <div class="row"> --}}
 
-            <div class="card">
-                <div class="card-body">
-                   <div class="container col-md-12" id="main">
+            {{-- <div class="card"> --}}
+                {{-- <div class="card-body"> --}}
+                   {{-- <div class="container col-md-12" id="main"> --}}
 
-                        <div class="row">
+                        {{-- <div class="row"> --}}
                             <div class="col-lg-9 col-md-9 col-sm-12 position-relative">
                                 <div class="card"
                                     style="border: 2px solid #7367F0; border-style: dashed; border-radius: 0;">
@@ -155,18 +155,17 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        {{-- </div> --}}
 
-                   </div>
+                   {{-- </div> --}}
 
-                </div>
-            </div>
-        </div>
+                {{-- </div>
+            </div> --}}
+        {{-- </div> --}}
 
 
-            <div class="row">
-
-            <div class="card">
+        <div class="row">
+            <div class="card" style="border: 2px solid #7367F0; border-style: dashed; border-radius: 0;">
                 <div class="card-body">
                    <div class="container col-md-12" id="main">
                         <div class="row" id="main_heading">
@@ -176,7 +175,9 @@
                                 </h3>
                             </div>
                         </div>
-                        <div class="row mt-3" id="sub_manu">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12" id="full_bar_graph">
+                            <div class="row mt-3" id="sub_manu">
                             <div class="col-md-4 sm-md-12">
                                 <p>
                                     <b> Due Amt</b>
@@ -202,24 +203,18 @@
                                 </h6>
                             </div>
                         </div>
-
-                        <div class="row">
-                                {{-- <div class="card chart-container full_width_graph">
-                                    <canvas id="chart"></canvas>
-                                </div> --}}
-                            <div class="col-md-12 col-sm-12" id="full_bar_graph">
-                                <div class="card chart-container">
-                                    <canvas id="chart"></canvas>
-                                </div>
+                        <div class="card chart-container">
+                            <canvas id="chart"></canvas>
+                        </div>
                             </div>
                             <div class="col-md-6 col-sm-12 d-none" id="pipe_chart_bar">
-                                <div id="pipe_chart_filter_data" class="row d-none">
+                                <div id="pipe_chart_filter_data" class="row">
                                     <div class="card">
                                         <div class="card-body">
                                             <div class="container">
-                                                <div class="row my-3">
+                                                <div class="row my-2">
                                                     <div class="col">
-                                                        <h4>Filter Data Show</h4>
+                                                        <h6 class="text-center" ><b> Filter Data Show </b></h6>
                                                     </div>
                                                 </div>
                                                 <div class="row py-2">
@@ -245,7 +240,19 @@
                             
                         </div>
 
-                        <div class="row table-responsive">
+                       
+                   </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="card" style="border: 2px solid #7367F0; border-style: dashed; border-radius: 0;">
+                <div class="card-body">
+                    <div class="row table-responsive">
                             <table id="example" class="table table-striped dt-complex-header table"
                                     style="width:100%">
                                 <thead>
@@ -314,8 +321,6 @@
                                 </tbody>
                             </table>
                         </div>
-                   </div>
-
                 </div>
             </div>
         </div>

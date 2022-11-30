3b7949faf9983d759db210886399c9e53b14c70f
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-11-30 15:42:17 +0500

    payment voucher table added

--- app/Http/Controllers/PaymentVocuherController.php
+++ app/Http/Controllers/PaymentVocuherController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\PaymentVoucherDatatable;
 use Illuminate\Http\Request;
 
 class PaymentVocuherController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(PaymentVoucherDatatable $dataTable, $site_id)
+    {
+        //
+        $data = [
+            'site_id' => $site_id,
+        ];
+        return $dataTable->with($data)->render('app.sites.payment-voucher.index', $data);
+    }
 }

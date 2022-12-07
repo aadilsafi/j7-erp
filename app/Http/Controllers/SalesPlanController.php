9acd4c399acfc5a74914f8d9066e899713e838fe
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-12-07 17:41:16 +0500

    sales plan changes updated

--- app/Http/Controllers/SalesPlanController.php
+++ app/Http/Controllers/SalesPlanController.php
@@ -6,7 +6,7 @@ use App\DataTables\ImportSalesPlanDataTable;
 use App\DataTables\SalesPlanDataTable;
 use App\Exceptions\GeneralException;
 use App\Imports\SalesPlanImport;
-use App\Models\{SalesPlan, Floor, LeadSource, Site, Stakeholder, TempSalePlan, Unit, User};
+use App\Models\{Country, SalesPlan, Floor, LeadSource, Site, Stakeholder, TempSalePlan, Unit, User};
 use Illuminate\Http\Request;
 use App\Models\SalesPlanTemplate;
 use App\Services\{
@@ -71,7 +71,7 @@ class SalesPlanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request, $site_id, $floor_id, $unit_id)
+    public function create(Request $request, $site_id, $floor_id = null, $unit_id = null)
     {
         if (!request()->ajax()) {
 
@@ -81,8 +81,8 @@ class SalesPlanController extends Controller
 
             $data = [
                 'site' => (new Site())->find(decryptParams($site_id)),
-                'floor' => (new Floor())->find(decryptParams($floor_id)),
-                'unit' => (new Unit())->with('status', 'type')->find(decryptParams($unit_id)),
+                // 'floor' => (new Floor())->find(decryptParams($floor_id)),
+                'unit' => (new Unit())->with('status', 'type')->where('status_id', '!=', 5)->get(),
                 'additionalCosts' => $this->additionalCostInterface->getAllWithTree($site_id),
                 'stakeholders' => $this->stakeholderInterface->getByAllWith(decryptParams($site_id), [
                     'stakeholder_types',
@@ -90,6 +90,7 @@ class SalesPlanController extends Controller
                 'stakeholderTypes' => StakeholderTypeEnum::values(),
                 'leadSources' => $this->leadSourceInterface->getByAll(decryptParams($site_id)),
                 'user' => auth()->user(),
+                'country' => Country::all(),
                 'customFields' => $customFields
 
             ];
@@ -106,7 +107,7 @@ class SalesPlanController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, $site_id, $floor_id, $unit_id)
+    public function store(Request $request, $site_id, $floor_id = null, $unit_id = null)
     {
         // dd($request->all());
         try {
@@ -120,8 +121,10 @@ class SalesPlanController extends Controller
                 return Redirect::back()->withErrors($validator);
             }
             $inputs = $request->input();
+            $floor_id = encryptParams($inputs['floor_id']);
+            $unit_id = encryptParams($inputs['unit_id']);
 
-            $record = $this->salesPlanInterface->store(decryptParams($site_id), decryptParams($floor_id), decryptParams($unit_id), $inputs);
+            $record = $this->salesPlanInterface->store(decryptParams($site_id), $inputs);
             return redirect()->route('sites.floors.units.sales-plans.index', ['site_id' => encryptParams(decryptParams($site_id)), 'floor_id' => encryptParams(decryptParams($floor_id)), 'unit_id' => encryptParams(decryptParams($unit_id))])->withSuccess('Sales Plan Saved!');
         } catch (GeneralException $ex) {
             Log::error($ex->getLine() . " Message => " . $ex->getMessage());
@@ -177,6 +180,12 @@ class SalesPlanController extends Controller
         //
     }
 
+    public function getUnitDetails(Request $request)
+    {
+        $unit = Unit::with('type')->find($request->unit_id);
+        return apiSuccessResponse([$unit, $unit->floor]);
+    }
+
     public function printPage($site_id, $floor_id, $unit_id, $sales_plan_id, $tempalte_id)
     {
         //
@@ -218,7 +227,7 @@ class SalesPlanController extends Controller
         return view('app.sites.floors.units.sales-plan.sales-plan-templates.' . $template->slug, compact('data'));
     }
 
-    public function ajaxGenerateInstallments(Request $request, $site_id, $floor_id, $unit_id)
+    public function ajaxGenerateInstallments(Request $request, $site_id, $floor_id = null, $unit_id = null)
     {
         $inputs = $request->input();
 

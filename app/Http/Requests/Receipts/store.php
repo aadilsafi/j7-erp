c6da42c4334c1edc6a7de2e205dc38356f7a8eab
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-12-16 16:16:00 +0500

    receipt bug fixes

--- app/Http/Requests/Receipts/store.php
+++ app/Http/Requests/Receipts/store.php
@@ -39,8 +39,10 @@ class store extends FormRequest
             $rules['receipts.*.bank_branch'] = ['required'];
             $rules['receipts.*.bank_address'] = ['required'];
             $rules['receipts.*.bank_contact_number'] = ['required'];
-            $rules['receipts.*.bank_account_number'] = ['required', 'numeric', Rule::unique('account_heads', 'code')];
-            $rules['receipts.*.bank_branch_code'] = ['required', 'numeric', Rule::unique('banks', 'branch_code')->ignore($this->input('receipts.*.bank_id')[0])];
+            if ($this->input('receipts.*.bank_id')[0] == 0) {
+                $rules['receipts.*.bank_account_number'] = ['required', 'numeric', Rule::unique('account_heads', 'code')];
+                $rules['receipts.*.bank_branch_code'] = ['required', 'numeric', Rule::unique('banks', 'branch_code')->ignore($this->input('receipts.*.bank_id')[0])];
+            }
             $rules['receipts.*.cheque_no'] = ['required'];
             $rules['attachment'] = ['required'];
         }
@@ -50,8 +52,10 @@ class store extends FormRequest
             $rules['receipts.*.bank_branch'] = ['required'];
             $rules['receipts.*.bank_address'] = ['required'];
             $rules['receipts.*.bank_contact_number'] = ['required'];
-            $rules['receipts.*.bank_account_number'] = ['required', 'numeric', Rule::unique('account_heads', 'code')];
-            $rules['receipts.*.bank_branch_code'] = ['required', 'numeric', Rule::unique('banks', 'branch_code')->ignore($this->input('receipts.*.bank_id')[0])];
+            if ($this->input('receipts.*.bank_id')[0] == 0) {
+                $rules['receipts.*.bank_account_number'] = ['required', 'numeric', Rule::unique('account_heads', 'code')];
+                $rules['receipts.*.bank_branch_code'] = ['required', 'numeric', Rule::unique('banks', 'branch_code')->ignore($this->input('receipts.*.bank_id')[0])];
+            }
             $rules['receipts.*.transaction_date'] = ['required'];
             $rules['receipts.*.online_instrument_no'] = ['required'];
             $rules['attachment'] = ['required'];

f35e83358213b7178c506e39360e669e59cdbdc6
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-12-23 16:35:07 +0500

    import changes

--- app/Http/Controllers/StakeholderImportController.php
+++ app/Http/Controllers/StakeholderImportController.php
@@ -538,7 +538,7 @@ class StakeholderImportController extends Controller
 
     public function saveImport(Request $request, $site_id, $type)
     {
-        // try {
+        try {
 
             // $validator = \Validator::make($request->all(), [
             //     'fields.*' => 'required',
@@ -549,174 +549,178 @@ class StakeholderImportController extends Controller
             // ]);
 
             // $validator->validate();
-            $model = new TempCompanyStakeholder();
-            $tempdata = $model->cursor();
-            $tempCols = $model->getFillable();
+            if ($type == 'c') {
+                $model = new TempCompanyStakeholder();
+                $tempdata = $model->cursor();
+                $tempCols = $model->getFillable();
 
-            $stakeholder = [];
-            $parentsCnics = [];
-            $parentsRelations = [];
+                $stakeholder = [];
+                $parentsCnics = [];
+                $parentsRelations = [];
 
-            foreach ($tempdata->chunk(30) as $Importkey => $importData) {
+                foreach ($tempdata->chunk(30) as $Importkey => $importData) {
 
-                DB::transaction(function () use ($site_id, $importData, $Importkey, $tempCols) {
-                    foreach ($importData as $key => $items) {
-                        $data[$key]['site_id'] = decryptParams($site_id);
-                        $data[$key]['stakeholder_as'] = 'c';
+                    DB::transaction(function () use ($site_id, $importData, $Importkey, $tempCols) {
+                        foreach ($importData as $key => $items) {
+                            $data[$key]['site_id'] = decryptParams($site_id);
+                            $data[$key]['stakeholder_as'] = 'c';
 
-                        foreach ($tempCols as $k => $field) {
-                            $data[$key][$field] = $items[$tempCols[$k]];
-                        }
-                        $data[$key]['is_imported'] = true;
-                        $data[$key]['full_name'] = $data[$key]['company_name'];
-                        $data[$key]['cnic'] = $data[$key]['registration'];
-                        if ($data[$key]['origin'] != "null") {
-                            $data[$key]['origin'] = 167;
-                        } else {
-                            $nationality = Country::whereRaw('LOWER(name) = (?)', [strtolower($data[$key]['origin'])])->first();
-                            if ($nationality) {
-                                $data[$key]['origin'] = $nationality->id;
-                            } else {
+                            foreach ($tempCols as $k => $field) {
+                                $data[$key][$field] = $items[$tempCols[$k]];
+                            }
+                            $data[$key]['is_imported'] = true;
+                            $data[$key]['full_name'] = $data[$key]['company_name'];
+                            $data[$key]['cnic'] = $data[$key]['registration'];
+                            if ($data[$key]['origin'] != "null") {
                                 $data[$key]['origin'] = 167;
-                            }
-                        }
-
-                        // residential address 
-
-                        if ($data[$key]['residential_country'] != "null") {
-                            $country = Country::whereRaw('LOWER(name) = (?)', [strtolower($data[$key]['residential_country'])])->first();
-                            if ($country) {
-                                $data[$key]['residential_country_id'] = $country->id;
                             } else {
-                                $data[$key]['residential_country_id'] = 167;
-                            }
-                        }
-
-                        if ($data[$key]['residential_state'] != "null") {
-                            $state = State::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['residential_state']))->first();
-                            if ($state) {
-                                $data[$key]['residential_state_id'] = $state->id;
-                            } else {
-                                $data[$key]['residential_state_id'] = 0;
-                            }
-                        }
-
-                        if ($data[$key]['residential_city'] != "null") {
-                            $city = City::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['residential_city']))->first();
-                            if ($city) {
-                                $data[$key]['residential_city_id'] = $city->id;
-                            } else {
-                                $data[$key]['residential_city_id'] = 0;
-                            }
-                        }
-
-                        if ($data[$key]['same_address_for_mailing'] == 'yes') {
-                            $data[$key]['mailing_address'] = $data[$key]['residential_address'];
-                            $data[$key]['mailing_address_type'] = $data[$key]['residential_address_type'];
-                            $data[$key]['mailing_country_id'] = $data[$key]['residential_country_id'];
-                            $data[$key]['mailing_state_id'] = $data[$key]['residential_state_id'];
-                            $data[$key]['mailing_city_id'] = $data[$key]['residential_city_id'];
-                            $data[$key]['mailing_postal_code'] = $data[$key]['residential_postal_code'];
-                        } else {
-                            // mailing address 
-
-                            if ($data[$key]['mailing_country'] != "null") {
-                                $country = Country::whereRaw('LOWER(name) = (?)', [strtolower($data[$key]['mailing_country'])])->first();
-                                if ($country) {
-                                    $data[$key]['mailing_country_id'] = $country->id;
+                                $nationality = Country::whereRaw('LOWER(name) = (?)', [strtolower($data[$key]['origin'])])->first();
+                                if ($nationality) {
+                                    $data[$key]['origin'] = $nationality->id;
                                 } else {
-                                    $data[$key]['mailing_country_id'] = 167;
+                                    $data[$key]['origin'] = 167;
                                 }
                             }
 
-                            if ($data[$key]['mailing_state'] != "null") {
-                                $state = State::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['mailing_state']))->first();
+                            // residential address 
+
+                            if ($data[$key]['residential_country'] != "null") {
+                                $country = Country::whereRaw('LOWER(name) = (?)', [strtolower($data[$key]['residential_country'])])->first();
+                                if ($country) {
+                                    $data[$key]['residential_country_id'] = $country->id;
+                                } else {
+                                    $data[$key]['residential_country_id'] = 167;
+                                }
+                            }
+
+                            if ($data[$key]['residential_state'] != "null") {
+                                $state = State::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['residential_state']))->first();
                                 if ($state) {
-                                    $data[$key]['mailing_state_id'] = $state->id;
+                                    $data[$key]['residential_state_id'] = $state->id;
+                                } else {
+                                    $data[$key]['residential_state_id'] = 0;
                                 }
                             }
 
-                            if ($data[$key]['mailing_city'] != "null") {
-                                $city = City::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['mailing_city']))->first();
+                            if ($data[$key]['residential_city'] != "null") {
+                                $city = City::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['residential_city']))->first();
                                 if ($city) {
-                                    $data[$key]['mailing_city_id'] = $city->id;
+                                    $data[$key]['residential_city_id'] = $city->id;
+                                } else {
+                                    $data[$key]['residential_city_id'] = 0;
                                 }
                             }
+
+                            if ($data[$key]['same_address_for_mailing'] == 'yes') {
+                                $data[$key]['mailing_address'] = $data[$key]['residential_address'];
+                                $data[$key]['mailing_address_type'] = $data[$key]['residential_address_type'];
+                                $data[$key]['mailing_country_id'] = $data[$key]['residential_country_id'];
+                                $data[$key]['mailing_state_id'] = $data[$key]['residential_state_id'];
+                                $data[$key]['mailing_city_id'] = $data[$key]['residential_city_id'];
+                                $data[$key]['mailing_postal_code'] = $data[$key]['residential_postal_code'];
+                            } else {
+                                // mailing address 
+
+                                if ($data[$key]['mailing_country'] != "null") {
+                                    $country = Country::whereRaw('LOWER(name) = (?)', [strtolower($data[$key]['mailing_country'])])->first();
+                                    if ($country) {
+                                        $data[$key]['mailing_country_id'] = $country->id;
+                                    } else {
+                                        $data[$key]['mailing_country_id'] = 167;
+                                    }
+                                }
+
+                                if ($data[$key]['mailing_state'] != "null") {
+                                    $state = State::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['mailing_state']))->first();
+                                    if ($state) {
+                                        $data[$key]['mailing_state_id'] = $state->id;
+                                    }
+                                }
+
+                                if ($data[$key]['mailing_city'] != "null") {
+                                    $city = City::whereRaw('LOWER(name) = (?)', strtolower($data[$key]['mailing_city']))->first();
+                                    if ($city) {
+                                        $data[$key]['mailing_city_id'] = $city->id;
+                                    }
+                                }
+                            }
+
+
+                            $data[$key]['created_at'] = $items->created_at;
+                            $data[$key]['updated_at'] = $items->updated_at;
+
+                            unset($data[$key]['company_name']);
+                            unset($data[$key]['is_dealer']);
+                            unset($data[$key]['is_vendor']);
+                            unset($data[$key]['is_customer']);
+                            unset($data[$key]['is_kin']);
+                            unset($data[$key]['regsitration']);
+                            unset($data[$key]['residential_country']);
+                            unset($data[$key]['residential_state']);
+                            unset($data[$key]['residential_city']);
+                            unset($data[$key]['mailing_country']);
+                            unset($data[$key]['mailing_state']);
+                            unset($data[$key]['mailing_city']);
+                            unset($data[$key]['same_address_for_mailing']);
+
+                            $stakeholder = Stakeholder::create($data[$key]);
+
+                            $stakeholdertype = [
+                                [
+                                    'stakeholder_id' => $stakeholder->id,
+                                    'type' => 'C',
+                                    'stakeholder_code' => 'C-00' . $stakeholder->id,
+                                    'status' => $items['is_customer'] ? 1 : 0,
+                                    'created_at' => now(),
+                                    'updated_at' => now(),
+                                ],
+                                [
+                                    'stakeholder_id' => $stakeholder->id,
+                                    'type' => 'V',
+                                    'stakeholder_code' => 'V-00' . $stakeholder->id,
+                                    'status' => $items['is_vendor'] ? 1 : 0,
+                                    'created_at' => now(),
+                                    'updated_at' => now(),
+                                ],
+                                [
+                                    'stakeholder_id' => $stakeholder->id,
+                                    'type' => 'D',
+                                    'stakeholder_code' => 'D-00' . $stakeholder->id,
+                                    'status' => $items['is_dealer'] ? 1 : 0,
+                                    'created_at' => now(),
+                                    'updated_at' => now(),
+                                ],
+                                [
+                                    'stakeholder_id' => $stakeholder->id,
+                                    'type' => 'K',
+                                    'stakeholder_code' => 'K-00' . $stakeholder->id,
+                                    'status' => $items['is_kin'] ? 1 : 0,
+                                    'created_at' => now(),
+                                    'updated_at' => now(),
+                                ],
+                                [
+                                    'stakeholder_id' => $stakeholder->id,
+                                    'type' => 'L',
+                                    'stakeholder_code' => 'L-00' . $stakeholder->id,
+                                    'status' => 1,
+                                    'created_at' => now(),
+                                    'updated_at' => now(),
+                                ]
+                            ];
+
+                            $stakeholder_type = StakeholderType::insert($stakeholdertype);
                         }
-                        
-
-                        $data[$key]['created_at'] = $items->created_at;
-                        $data[$key]['updated_at'] = $items->updated_at;
-
-                        unset($data[$key]['company_name']);
-                        unset($data[$key]['is_dealer']);
-                        unset($data[$key]['is_vendor']);
-                        unset($data[$key]['is_customer']);
-                        unset($data[$key]['is_kin']);
-                        unset($data[$key]['regsitration']);
-                        unset($data[$key]['residential_country']);
-                        unset($data[$key]['residential_state']);
-                        unset($data[$key]['residential_city']);
-                        unset($data[$key]['mailing_country']);
-                        unset($data[$key]['mailing_state']);
-                        unset($data[$key]['mailing_city']);
-                        unset($data[$key]['same_address_for_mailing']);
-
-                        $stakeholder = Stakeholder::create($data[$key]);
-
-                        $stakeholdertype = [
-                            [
-                                'stakeholder_id' => $stakeholder->id,
-                                'type' => 'C',
-                                'stakeholder_code' => 'C-00' . $stakeholder->id,
-                                'status' => $items['is_customer'] ? 1 : 0,
-                                'created_at' => now(),
-                                'updated_at' => now(),
-                            ],
-                            [
-                                'stakeholder_id' => $stakeholder->id,
-                                'type' => 'V',
-                                'stakeholder_code' => 'V-00' . $stakeholder->id,
-                                'status' => $items['is_vendor'] ? 1 : 0,
-                                'created_at' => now(),
-                                'updated_at' => now(),
-                            ],
-                            [
-                                'stakeholder_id' => $stakeholder->id,
-                                'type' => 'D',
-                                'stakeholder_code' => 'D-00' . $stakeholder->id,
-                                'status' => $items['is_dealer'] ? 1 : 0,
-                                'created_at' => now(),
-                                'updated_at' => now(),
-                            ],
-                            [
-                                'stakeholder_id' => $stakeholder->id,
-                                'type' => 'K',
-                                'stakeholder_code' => 'K-00' . $stakeholder->id,
-                                'status' => $items['is_kin'] ? 1 : 0,
-                                'created_at' => now(),
-                                'updated_at' => now(),
-                            ],
-                            [
-                                'stakeholder_id' => $stakeholder->id,
-                                'type' => 'L',
-                                'stakeholder_code' => 'L-00' . $stakeholder->id,
-                                'status' => 1,
-                                'created_at' => now(),
-                                'updated_at' => now(),
-                            ]
-                        ];
-
-                        $stakeholder_type = StakeholderType::insert($stakeholdertype);
-                    }
-                });
-
+                    });
+                }
             }
-            // ImportStakeholders::dispatch($site_id);
+
+            if ($type == 'i') {
+                ImportStakeholders::dispatch($site_id);
+            }
 
             return redirect()->route('sites.stakeholders.index', ['site_id' => encryptParams(decryptParams($site_id))])->withSuccess('Data will be imported Shortly.');
-        // } catch (\Throwable $th) {
-        //     return redirect()->route('sites.stakeholders.index', ['site_id' => encryptParams(decryptParams($site_id))])->withdanger('Somethings Went wrong');
-        // }
+        } catch (\Throwable $th) {
+            return redirect()->route('sites.stakeholders.index', ['site_id' => encryptParams(decryptParams($site_id))])->withdanger('Somethings Went wrong');
+        }
     }
 }

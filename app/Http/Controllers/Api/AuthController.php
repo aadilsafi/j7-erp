35dba9616edf9b6dada3f996b8e9de714de1660e
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-12-27 12:09:39 +0500

    import bug fixes

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\Api\ApiAuthService;
+use Illuminate\Http\Request;
+
+
+class AuthController extends Controller
+{
+    protected $apiAuthService;
+
+    public function __construct(ApiAuthService $apiAuthService)
+    {
+        $this->apiAuthService = $apiAuthService;
+    }
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     */
+    public function login(Request $request)
+    {
+        $data = $request->only(
+            'email',
+            'password',
+        );
+        return $this->apiAuthService->login($data, $request);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Tokens Revoked'
+        ];
+    }
+
+    public function checkAuth()
+    {
+        if (auth()->check()) {
+            return response()->json(['message'  =>  'success']);
+        } else {
+            return response('error', 401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('test', function () {
+        return response()->json(['message' => 'success']);
+    });
+    Route::post('login', [AuthController::class, 'login']);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('checkAuth', [AuthController::class, 'checkAuth']);
 });

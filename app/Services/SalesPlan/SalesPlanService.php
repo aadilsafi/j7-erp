b71c0ed82c7b5f9d89c75075799c180835894004
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-12-29 16:55:12 +0500

    bug fixes

--- app/Services/SalesPlan/SalesPlanService.php
+++ app/Services/SalesPlan/SalesPlanService.php
@@ -117,7 +117,7 @@ class SalesPlanService implements SalesPlanInterface
                     'referred_by' => $individual['referred_by'],
                     'source' => $individual['source'] ?? 0,
                     'date_of_birth' => $individual['dob'],
-                    'is_local' => $individual['is_local'],
+                    'is_local' => isset($individual['is_local']) ? $individual['is_local'] : 0,
                     'nationality' => $individual['nationality'],
                 ];
             } else if ($stakeholder_as == 'c') {

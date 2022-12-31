3de7716820a1a8e2d473a493210a206a7b4cf4e9
iftikhar.islootech <iftikhar.islootech@gmail.com>
2022-12-31 18:18:45 +0500

    logo updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,19 +40,19 @@
                         </g>
                     </g>
                 </svg> --}}
-                <h2 class="brand-text text-primary ms-1">{{ env('APP_NAME') }}</h2>
+                {{-- <h2 class="brand-text text-primary ms-1">{{ env('APP_NAME') }}</h2> --}}
             </a>
             <!-- /Brand logo-->
             <!-- Left Text-->
             <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
                 <div class="w-100 d-lg-flex align-items-center justify-content-center px-5"><img class="img-fluid"
-                        src="{{ asset('app-assets') }}/images/pages/login-v2.svg" alt="Login V2" /></div>
+                        src="{{ asset('app-assets') }}/images/erp.png" alt="Login V2" /></div>
             </div>
             <!-- /Left Text-->
             <!-- Login-->
             <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-                    <h2 class="card-title fw-bold mb-1">Welcome to - {{ env('APP_NAME') }}! 👋</h2>
+                    <h2 class="card-title fw-bold mb-1 brand-text">Welcome to - {{ Str::title(env('APP_NAME')) }}! 👋</h2>
                     <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
                     <div class="">
                         {{ view('app.layout.alerts') }}

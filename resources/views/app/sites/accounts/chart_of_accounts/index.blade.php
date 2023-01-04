a23bb029c823f68882e5c267759f0c7ed2a68910
asmat2991 <56365538+asmat2991@users.noreply.github.com>
2023-01-04 13:11:55 +0500

    chart of accounts new style

--- resources/views/app/sites/accounts/chart_of_accounts/index.blade.php
+++ resources/views/app/sites/accounts/chart_of_accounts/index.blade.php
@@ -124,7 +124,87 @@
             position: unset;
         }
     </style>
+
+    <style>
+        .tree,
+        .tree ul {
+            margin: 0;
+            padding: 0;
+            list-style: none
+        }
+
+        .tree ul {
+            margin-left: 1em;
+            position: relative
+        }
+
+        .tree ul ul {
+            margin-left: .5em
+        }
+
+        .tree ul:before {
+            content: "";
+            display: block;
+            width: 0;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            border-left: 1px solid
+        }
+
+        .tree li {
+            margin: 0;
+            padding: 0 1em;
+            line-height: 2em;
+            color: #369;
+            font-weight: 700;
+            position: relative
+        }
+
+        .tree ul li:before {
+            content: "";
+            display: block;
+            width: 10px;
+            height: 0;
+            border-top: 1px solid;
+            margin-top: -1px;
+            position: absolute;
+            top: 1em;
+            left: 0
+        }
+
+        .tree ul li:last-child:before {
+            background: #fff;
+            height: auto;
+            top: 1em;
+            bottom: 0
+        }
+
+        .indicator {
+            margin-right: 5px;
+        }
+
+        .tree li a {
+            text-decoration: none;
+            color: #369;
+        }
+
+        .tree li button,
+        .tree li button:active,
+        .tree li button:focus {
+            text-decoration: none;
+            color: #369;
+            border: none;
+            background: transparent;
+            margin: 0px 0px 0px 0px;
+            padding: 0px 0px 0px 0px;
+            outline: 0;
+        }
+    </style>
 @endsection
+
+
 
 @section('breadcrumbs')
     <div class="content-header-left col-md-9 col-12 mb-2">
@@ -139,8 +219,117 @@
     </div>
 @endsection
 
+
+
+
+
+
+
+
+
+
+
+
 @section('content')
     <section class="app-user-view-connections">
+
+
+        <ul id="tree1">
+            </p>
+            <li>
+                <a href="#">1 Asset</a>
+                <ul>
+                    <li>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </li>
+
+                    <li class="ps-2"><a href="#">2 Non-current Asset</a>
+
+                        <ul>
+
+                            <li>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">First</th>
+                                            <th scope="col">Last</th>
+                                            <th scope="col">Handle</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+
+                            </li>
+                            <li>
+                                <a href="#">3 TECH</a>
+                                <ul>
+                                    <li>
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">#</th>
+                                                    <th scope="col">First</th>
+                                                    <th scope="col">Last</th>
+                                                    <th scope="col">Handle</th>
+                                                </tr>
+                                            </thead>
+                                        </table>
+
+                                    </li>
+                                    <li>
+                                        <a href="#">4 Level</a>
+                                        <ul>
+                                            <li>
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">#</th>
+                                                            <th scope="col">First</th>
+                                                            <th scope="col">Last</th>
+                                                            <th scope="col">Handle</th>
+                                                        </tr>
+                                                    </thead>
+                                                </table>
+
+                                            </li>
+                                            <li>
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">#</th>
+                                                            <th scope="col">First</th>
+                                                            <th scope="col">Last</th>
+                                                            <th scope="col">Handle</th>
+                                                        </tr>
+                                                    </thead>
+                                                </table>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+
+
+
+
+
+                        </ul>
+
+                    </li>
+                </ul>
+            </li>
+
+        </ul>
+
         <div class="row removeInvalidMessages">
             <div class="col-xl-12 col-lg-12">
 
@@ -177,7 +366,6 @@
                                 </div>
                                 <!-- Right Sidebar ends -->
                                 {{-- example according  --}}
-
                                 <div class="accordion accordion-flush" id="accordionFlushExample">
                                     @foreach ($account_of_heads->where('level', 1) as $key_first => $account_of_head)
                                         @php
@@ -187,6 +375,7 @@
                                             $value_frist_value = 0;
                                         @endphp
                                         <div class="accordion-item ">
+
                                             <h2 class="accordion-header" id="flush-{{ $key_first }}">
                                                 <button class="new_according_bg accordion-button collapsed" type="button"
                                                     data-bs-toggle="collapse"
@@ -680,4 +869,73 @@
 
 @section('custom-js')
 
+    <script>
+        $.fn.extend({
+            treed: function(o) {
+
+                var openedClass = 'glyphicon-minus-sign';
+                var closedClass = 'glyphicon-plus-sign';
+
+                if (typeof o != 'undefined') {
+                    if (typeof o.openedClass != 'undefined') {
+                        openedClass = o.openedClass;
+                    }
+                    if (typeof o.closedClass != 'undefined') {
+                        closedClass = o.closedClass;
+                    }
+                };
+
+                //initialize each of the top levels
+                var tree = $(this);
+                tree.addClass("tree");
+                tree.find('li').has("ul").each(function() {
+                    var branch = $(this); //li with children ul
+                    branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
+                    branch.addClass('branch');
+                    branch.on('click', function(e) {
+                        if (this == e.target) {
+                            var icon = $(this).children('i:first');
+                            icon.toggleClass(openedClass + " " + closedClass);
+                            $(this).children().children().toggle();
+                        }
+                    })
+                    branch.children().children().toggle();
+                });
+                //fire event from the dynamically added icon
+                tree.find('.branch .indicator').each(function() {
+                    $(this).on('click', function() {
+                        $(this).closest('li').click();
+                    });
+                });
+                //fire event to open branch if the li contains an anchor instead of text
+                tree.find('.branch>a').each(function() {
+                    $(this).on('click', function(e) {
+                        $(this).closest('li').click();
+                        e.preventDefault();
+                    });
+                });
+                //fire event to open branch if the li contains a button instead of text
+                tree.find('.branch>button').each(function() {
+                    $(this).on('click', function(e) {
+                        $(this).closest('li').click();
+                        e.preventDefault();
+                    });
+                });
+            }
+        });
+
+        //Initialization of treeviews
+
+        $('#tree1').treed();
+
+        $('#tree2').treed({
+            openedClass: 'glyphicon-folder-open',
+            closedClass: 'glyphicon-folder-close'
+        });
+
+        $('#tree3').treed({
+            openedClass: 'glyphicon-chevron-right',
+            closedClass: 'glyphicon-chevron-down'
+        });
+    </script>
 @endsection

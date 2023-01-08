a5b909d295c5264775d85adeb6d78230bcb85532
Aadil Safi <aadilxafi@gmail.com>
2023-01-08 17:19:41 +0500

    testing trail balance

--- app/Http/Controllers/TrialBalanceController.php
+++ app/Http/Controllers/TrialBalanceController.php
@@ -126,8 +126,16 @@ class TrialBalanceController extends Controller
                 }
 
                 if ($i > 1) {
-                    $new_starting_balance = ($ending_balance + $starting_balance[$starting_balance_index - 1]);
-                    $starting_balance[$starting_balance_index] = $new_starting_balance;
+                    // $closingBalance = ;
+                    if($i==2){
+                        $new_starting_balance = ($closingBalance + $starting_balance[$starting_balance_index - 1]);
+                        $starting_balance[$starting_balance_index] = $new_starting_balance;
+                    }
+                    else{
+                        $new_starting_balance = ($ending_balance + $starting_balance[$starting_balance_index - 1]);
+                        $starting_balance[$starting_balance_index] = $new_starting_balance;
+                    }
+
                 }
 
 

326aeaf1fe3972136bfd45d508e43b1f2b7109d2
iftikhar.islootech <iftikhar.islootech@gmail.com>
2023-01-11 13:13:40 +0500

    Receipts import changes

--- app/Imports/ReceiptsImport.php
+++ app/Imports/ReceiptsImport.php
@@ -38,7 +38,7 @@ class ReceiptsImport implements ToModel, WithChunkReading, WithBatchInserts, Wit
             ->where('unit_id', $unitId->id)
             ->where('total_price', $row['total_price'])
             ->where('down_payment_total', $row['down_payment_total'])
-            ->where('validity', Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['validity']))->format('Y-m-d'))
+            ->where('approved_date', Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['sales_plan_approval_date']))->format('Y-m-d'))
             ->first();
         if (!$salePlan) {
             $error = ['Could not find sales Plan'];
@@ -53,7 +53,7 @@ class ReceiptsImport implements ToModel, WithChunkReading, WithBatchInserts, Wit
             'stakeholder_cnic' => $row['stakeholder_cnic'],
             'total_price' => $row['total_price'],
             'down_payment_total' => $row['down_payment_total'],
-            'validity' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['validity']))->format('Y-m-d'),
+            'validity' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['sales_plan_approval_date']))->format('Y-m-d'),
             'mode_of_payment' => $row['mode_of_payment'],
             'amount' => $row['amount'],
             'cheque_no' => $row['cheque_no'],
@@ -84,7 +84,7 @@ class ReceiptsImport implements ToModel, WithChunkReading, WithBatchInserts, Wit
             'stakeholder_cnic' =>  ['required', 'exists:App\Models\Stakeholder,cnic'],
             'total_price' =>  ['required', 'numeric', 'gt:0'],
             'down_payment_total' =>  ['required', 'numeric', 'gt:0'],
-            'validity' =>  ['required'],
+            'sales_plan_approval_date' =>  ['required'],
             'bank_acount_number' => ['sometimes', 'nullable', 'exists:banks,account_number'],
             'mode_of_payment' =>  ['required'],
             'amount' => ['required'],
